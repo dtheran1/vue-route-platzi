3d7a77cf802cff24fea21f0e548cd7ac5619489f
Dtheran <dtheran1@gmail.com>
2023-01-12 11:58:01 -0500

    diferentes tipos de navegacion con router

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,20 @@
 <script setup>
   // import TheWelcome from '../components/TheWelcome.vue'
   import { ref } from 'vue';
+  import { useRouter } from 'vue-router';
+
+  const router = useRouter();
   const username = ref('');
   const ingresar = () => {
-    console.log(username.value);
-    $router.push('/chats');
+    router.replace('/chats'); // Reemplaza la ruta de navegacion y no deja puntos en el historial del navegador.
+    router.push('/chats'); // este si deja un punto en el historial de navegacion
+    router.push({
+      path: '/chats',
+      query: {
+        variable1: 1, //Aqui podemos definir las diferentes queries
+      },
+      replace: true, // podemos agregar la propiedad para que funcione como sin dejar puntos de navegacion.
+    });
   };
 </script>
 
@@ -12,8 +22,6 @@
   <main>
     <h3>Introduce tu nombre de usuario</h3>
     <input type="text" placeholder="Username" v-model="username" />
-    <router-link to="/chats">
-      <button>Ingresar</button>
-    </router-link>
+    <button @click="ingresar">Iniciar Sesion</button>
   </main>
 </template>
